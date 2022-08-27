0163471ceabd6c2a1faad26e9ed1334bcc9b81dc
diegoBreeg <99042053+DiegoBreeg@users.noreply.github.com>
2022-08-27 13:18:16 -0300

    Create tipo_de_triangulo.js

---
+++ tipo_de_triangulo/tipo_de_triangulo.js
@@ -0,0 +1,14 @@
+function descobre_tipo_triangulo(a, b, c){
+    var arrey = [a, b, c];
+    var counts = {};
+    arrey.forEach(function(x) { counts[x] = (counts[x] || 0)+1; });
+    console.log(counts);
+    const response = {
+        1: 'Equilátero', //três lados iguais
+        2: 'Isóceles', //dois lados iguais
+        3: 'Escaleno', //todos os lados diferentes
+    }
+    console.log(response[Object.keys(counts).length])
+}
+
+descobre_tipo_triangulo(3, 3, 3)
